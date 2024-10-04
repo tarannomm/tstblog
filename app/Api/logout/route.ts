85f0704cb8653945113f84947e101c1b2e60e673
tarannom azimi <tarannom.azimi99@gmail.com>
2024-10-04 23:43:30 +0330

    fix logout

--- app/Api/logout/route.ts
+++ app/Api/logout/route.ts
@@ -2,18 +2,17 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: NextApiRequest) {
-    // ایجاد پاسخ خروج
+     
     const response = NextResponse.json(
         { message: 'شما با موفقیت خارج شدید' },
         { status: 200 }
     );
-
-    // پاک کردن توکن از کوکی‌ها
+ 
     response.cookies.set('token', '', {
         httpOnly: true,
         path: '/',
         expires: new Date(0), 
-        sameSite: 'Strict', 
+        sameSite: 'strict', 
         secure: process.env.NODE_ENV === 'production', 
     });
 
